76c6c9567ff52980b38590dccb395be776500a58
Prashant <hadaprashant2002@gmail.com>
2024-08-08 02:09:41 +0530

    added main functionality of map and location

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const http2 = require("http2");
+const http = require("http");
 const path = require("path");
 const { Socket } = require("socket.io");
 const socketio = require("socket.io")
@@ -7,14 +7,28 @@ const socketio = require("socket.io")
 const PORT = 3000;
 const app = express();
 
-const server = http2.createServer(app);
+const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname,"public")));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection",function (socket){
+    console.log("user connected with socket.id: ", socket.id);
+    socket.on("send-location", (data)=>{
+        io.emit("receive-location",{id:socket.id, ...data});
+    })
+
+    socket.on("disconnect",()=>{
+        console.log(`User ${socket.id} has disconnected`);
+        io.emit("user-disconnected",socket.id);
+    })
+})
 
 app.get("/",(req,res)=>{
-    return res.send("Hey");
+    // res.send("hey");
+    res.render("index");
 })
 
 server.listen(PORT,()=>{
